fb762e2fdf23fc77866035c03784efcdadcb8e09
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2025-05-25 16:21:08 +0530

    feat: created calculator

--- index1.js
+++ index1.js
@@ -1,3 +1,13 @@
 console.log("index1 in test branch");
 
 const a = 20;
+
+const calculator = (num1, num2, symbol) => {
+  switch(symbol){
+    case '+': return num1 + num2;
+    case '-': return num1 - num2;
+    default: return undefined;
+  }
+}
+
+console.log(calculator(2, 3, '+'));
